f9da6ab81a6c87e7aa28f6f08c96b160c970b2f8
Philip Damra <djheru@gmail.com>
2016-06-03 23:25:24 -0400

    cleanup course list

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,8 +1,8 @@
 import React, {PropTypes} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import {Link} from 'react-router';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component{
   constructor(props, context) {
@@ -27,16 +27,12 @@ class CoursesPage extends React.Component{
     this.setState({course});
   }*/
 
-  courseRow(course, index) {
-    return (
-      <div key={index}>{course.title}</div>
-    );
-  }
   render() {
+    const {courses} = this.props;
     return (
       <div className="jumbotron">
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
+        <CourseList courses={courses} />
       </div>
     );
   }
