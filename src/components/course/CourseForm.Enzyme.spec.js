bfafc4d301e61aa70c202e60defb74284eae9102
Philip Damra <djheru@gmail.com>
2016-06-05 17:14:07 -0400

    Adding testing of React components via React-Test-Utils and Enzyme

---
+++ src/components/course/CourseForm.Enzyme.spec.js
@@ -0,0 +1,38 @@
+import expect from 'expect';
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import TestUtils from 'react-addons-test-utils';
+import CourseForm from './CourseForm';
+
+function setup(passedProps = {}) {
+  let defaultProps = {
+    course: {},
+    saving: false,
+    errors: {},
+    saving: false,
+    onSave: () => {},
+    onChange: () => {}
+  };
+  let props = Object.assign(defaultProps, passedProps);
+  return shallow(<CourseForm {...props} />);
+}
+
+describe('CourseForm via Enzyme', () => {
+
+  it('renders form and h1', () => {
+    const wrapper = setup();
+    expect(wrapper.find('form').length).toBe(1);
+    expect(wrapper.find('h1').text()).toEqual('Manage Course');
+  });
+
+  it('renders the save button with the label "Save" before saving', () => {
+    const wrapper = setup();
+    expect(wrapper.find('input').props().value).toBe('Save');
+  });
+
+  it('renders the save button with the label "Saving..." when saving', () => {
+    const wrapper = setup({saving: true});
+    expect(wrapper.find('input').props().value).toBe('Saving...');
+  });
+
+});
---
+++ src/components/course/CourseForm.ReactTestUtils.spec.js
@@ -0,0 +1,44 @@
+import expect from 'expect';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import CourseForm from './CourseForm';
+
+function setup(passedProps = {}) {
+  let defaultProps = {
+    course: {},
+    saving: false,
+    errors: {},
+    saving: false,
+    onSave: () => {},
+    onChange: () => {}
+  };
+  let props = Object.assign(defaultProps, passedProps);
+  let renderer = TestUtils.createRenderer();
+  renderer.render(<CourseForm {...props} />);
+  let output = renderer.getRenderOutput();
+
+  return {props, output, renderer};
+}
+
+describe('CourseForm via React Test Utils', () => {
+
+  it('renders a form and an h1', () => {
+    const { output } = setup();
+    expect(output.type).toBe('form');
+    let [h1] = output.props.children;
+    expect(h1.type).toBe('h1');
+  });
+
+  it('renders the save button with the label "Save" before saving', () => {
+    const { output } = setup();
+    let [,,,,,saveBtn] = output.props.children;
+    expect(saveBtn.props.value).toBe('Save');
+  });
+
+  it('renders the save button with the label "Saving..." when saving', () => {
+    const {output} = setup({saving: true});
+    let [,,,,,saveBtn] = output.props.children;
+    expect(saveBtn.props.value).toBe('Saving...');
+  });
+
+});
