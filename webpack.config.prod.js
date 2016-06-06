5c51c9c8992283304a401b45bc8ddd1eddbea206
Philip Damra <djheru@gmail.com>
2016-06-05 22:04:29 -0400

    Production build

---
+++ webpack.config.prod.js
@@ -0,0 +1,40 @@
+import webpack from 'webpack';
+import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const GLOBALS = {
+  'process.env.NODE_ENV': JSON.stringify('production')
+};
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: './src/index',
+  target: 'web',
+  output: {
+    path: path.join(__dirname, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),//optimizes the order that the files are bundled in for minification
+    new webpack.DefinePlugin(GLOBALS),//pass environment variables
+    new ExtractTextPlugin('styles.css'),//extracts from loader below into this separate file
+    new webpack.optimize.DedupePlugin(),//eliminates duplicate packages
+    new webpack.optimize.UglifyJsPlugin()//make it ugly
+  ],
+  module: {
+    loaders: [ // Which files do we webpack?
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /(\.css)$/, loader: ExtractTextPlugin.extract("css?sourceMap")},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  }
+};
