c1a541ff83f761027fabe15a71b8eeec17ed6cff
Philip Damra <djheru@gmail.com>
2016-06-06 23:14:08 -0400

    update deps and add devTools

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
@@ -7,9 +7,12 @@ export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(
-      thunk,
-      reduxImmutableStateInvariant()//only for dev, not prod
+    compose(
+      applyMiddleware(
+        thunk,
+        reduxImmutableStateInvariant()//only for dev, not prod
+      ),
+      window.devToolsExtension ? window.devToolsExtension(): f => f
     )
   );
 }
