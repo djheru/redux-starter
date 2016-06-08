18fd5323698d8c722abd0c71b11d2e3a2ca87a6e
Philip Damra <djheru@gmail.com>
2016-06-08 18:41:31 -0400

    added better header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,12 +3,43 @@ import {Link, IndexLink} from 'react-router';
 
 const Header = ({loading}) => {
   return (
-    <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      &nbsp; | &nbsp;
-      <Link to="/courses" activeClassName="active">Courses</Link>
-      &nbsp; | &nbsp;
-      <Link to="/about" activeClassName="active">About</Link>
+    <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div className="container-fluid">
+
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#navbar"
+            aria-expanded="false"
+            aria-controls="navbar">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <Link className="navbar-brand" to="/">React-Redux Boilerplate</Link>
+        </div>
+
+        <div id="navbar" className="collapse navbar-collapse">
+
+          {/*<NavDrop />*/}
+
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/courses">Courses</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+
+      </div>
     </nav>
   );
 };
